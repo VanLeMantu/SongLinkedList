d8ef690c4f336c6c78812ec94d778f92c9052b96
HUYENMY <huyenmyp02012003@gmail.com>
2024-04-02 13:24:30 +0700

    update Read song from file

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -1,133 +1,125 @@
-#include <stdio.h>
+﻿#include <stdio.h>
+#include<string.h>
 #include <stdlib.h>
-#include <string.h>
 #include<conio.h>
-
 struct Song {
 	char NameSong[31];
 	char Musician[26];
 	char Singer[26];
 	int Length;
 };
-
 struct SNode {
-	struct Song info;
-	struct SNode* next;
+	Song info;
+	SNode* Next;
 };
-
 struct SList {
-	struct SNode* Head;
-	struct SNode* Tail;
+	SNode* Head;
+	SNode* Tail;
 };
-
-void initSList(struct SList* sl) {
-	sl->Head = NULL;
-	sl->Tail = NULL;
+void initSList(SList& sl) {
+	sl.Head = NULL;
+	sl.Tail = NULL;
 }
-
-struct SNode* create_SNode(struct Song* x) {
-	struct SNode* p = (struct SNode*)malloc(sizeof(struct SNode));
-	if (p == NULL) {
-		printf("\n khong du bo nho de cap phap ");
-		return NULL;
-	}
-	p->info = *x;
-	p->next = NULL;
-	return p;
+bool isEmpty(SList& sl) {
+	if (sl.Head == NULL)
+		return true;
+	return false;
 }
-
-void show_Menu1() {
+void show_Menu() {
 	printf("\n-----------------------------\n");
 	printf("\n 0: to end program ");
 	printf("\n 1: tao mot bai hat ");
 	printf("\n 2: doc file ");
 	printf("\n-----------------------------\n");
 }
-
-void input_Song(struct Song* x) {
-	printf("\n Nhap ten bai hat: ");
-	getchar();
-	fgets(x->NameSong, sizeof(x->NameSong), stdin);
+void input_Song(Song& x) {
+	printf("\n nhap ten bai hat: ");
+	fgets(x.NameSong, sizeof(x.NameSong), stdin);
 	printf("\n nhap ten nghe si: ");
-	fgets(x->Musician, sizeof(x->Musician), stdin);
+	fgets(x.Musician, sizeof(x.Musician), stdin);
 	printf("\n nhap ten ca si: ");
-	fgets(x->Singer, sizeof(x->Singer), stdin);
+	fgets(x.Singer, sizeof(x.Singer), stdin);
 	printf("\n nhap do dai cua bai hat: ");
-	scanf_s("%d", &x->Length);
+	scanf("%d", &x.Length);
+	getchar(); 
 }
-
-void show_Song(struct Song x) {
+void show_Song(Song x) {
 	printf("%-31s%-26s%-26s%10d\n", x.NameSong, x.Musician, x.Singer, x.Length);
 }
-
-void show_SNode(struct SNode* p) {
+void show_SNode(SNode* p) {
 	show_Song(p->info);
 }
-
-static void addSNodeToList(struct SList* sl, struct SNode* node) {
-	if (sl->Head == NULL) {
-		sl->Head = node;
-		sl->Tail = node;
+SNode* create_SNode(Song& x) {
+	SNode* p = new SNode;
+	if (p == NULL) {
+		printf("\n khong du bo nho de cap phat ");
+		return NULL;
+	}
+	p->info = x;
+	p->Next - NULL;
+	return p;
+}
+void addSNodetoList(SList& sl, SNode* p) {
+	if (isEmpty(sl)) {
+		sl.Head = sl.Tail = p;
 	}
 	else {
-		sl->Tail->next = node;
-		sl->Tail = node;
+		sl.Tail->Next = p;
+		sl.Tail = p;
 	}
 }
-
-static void readSongsFromFile(struct SList* sl, const char* filename) {
-	FILE* file = NULL;
-	errno_t err = fopen_s(&file, filename, "r");
-	if (file == NULL) {
-		printf("\n Unable to open file: %s", filename);
+void readSongFromFile(SList& sl, const char* filename) {
+	FILE* fi = NULL;
+	fopen_s(&fi, filename, "r");
+	if (fi == NULL) {
+		printf("\n khong the mo file %s", filename);
 		return;
 	}
-
-	char buffer[256];
-	while (fgets(buffer, sizeof(buffer), file)) {
-		struct Song song {};
-		if (sscanf_s(buffer, "%30[^#]#%25[^#]#%25[^#]#%d", song.NameSong, (unsigned)_countof(song.NameSong), song.Musician, (unsigned)_countof(song.Musician), song.Singer, (unsigned)_countof(song.Singer), &song.Length) == 4) {
-			struct SNode* node = create_SNode(&song);
-			addSNodeToList(sl, node);
+	char buffer[100];
+	
+	while (fgets(buffer, sizeof(buffer), fi)) {
+		Song x;
+		if (sscanf_s(buffer, "%[^#]%*c%[^#]%*c%[^#]%*c%d", x.NameSong, (unsigned)_countof(x.NameSong), x.Musician, (unsigned)_countof(x.Musician), x.Singer, (unsigned)_countof(x.Singer), &x.Length) == 4) {
+			SNode* p = create_SNode(x);
+			addSNodetoList(sl, p);
 		}
 		else {
-			printf("\nFailed to parse song from line: %s", buffer);
+			printf("\n falied ");
 		}
 	}
-
-	fclose(file);
+	fclose(fi);
 }
-
-static void printAllSongs(struct SList* sl) {
-	struct SNode* current = sl->Head;
-
-	while (current != NULL) {
-		show_Song(current->info);
-		current = current->next;
+void prinftAllSong(SList sl) {
+	SNode* p = sl.Head;
+	if (p == NULL) {
+		printf("\n danh sach rong: ");
+		return;
+	}
+	while (p) {
+		show_SNode(p);
 	}
 }
-
 void bai1() {
 	int choice;
-	struct Song song;
-	struct SList sl;
-	initSList(&sl);
+	Song song;
+	SList sl;
+	initSList(sl);
 
 	do {
-		show_Menu1();
+		show_Menu();
 		printf("\n your choice :");
 		scanf_s("%d", &choice);
 
 		switch (choice) {
 		case 1: {
-			input_Song(&song);
-			struct SNode* p = create_SNode(&song);
+			input_Song(song);
+			SNode* p = create_SNode(song);
 			show_SNode(p);
 			break;
 		}
 		case 2: {
-			readSongsFromFile(&sl, "ListSong.txt");
-			printAllSongs(&sl);
+			readSongFromFile(sl, "ListSong,txt");
+			prinftAllSong(sl);
 			break;
 		}
 		default:
@@ -135,8 +127,8 @@ void bai1() {
 		}
 	} while (choice != 0);
 }
-
 int main() {
 	bai1();
+
 	return 0;
 }

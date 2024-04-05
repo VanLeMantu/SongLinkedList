92eedada3c1e8ccc0ef2243da52190e651095237
HUYENMY <huyenmyp02012003@gmail.com>
2024-04-05 23:16:29 +0700

    update stack

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -1,278 +1,270 @@
-﻿#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include<time.h>
-typedef int ItemType;
-struct SNode {
-	ItemType coeff;
-	ItemType  degree;
-	SNode* next;
+﻿#include <iostream>
+#include <cstdlib>
+#include <time.h>
+using namespace std;
+typedef char ItemType;
+struct StackNode {
+	ItemType info;
+	StackNode* next;
 };
-struct SList {
-	SNode* Head;
-	SNode* Tail;
+struct Stack {
+	StackNode* top;
 };
-void initSList(SList& sl) {
-	sl.Head = NULL;
-	sl.Tail = NULL;
+void  initStack(Stack& st) {
+	st.top = NULL;
 }
-bool isEmpty(SList& sl) {
-	if (sl.Head == NULL)
-		return true;
-	return false;
-}
-SNode* create_SNode(ItemType& coeff, ItemType& degree) {
-	SNode* p = new SNode;
-	if (p == NULL) {
-		printf( "\n khong du bo nho de cap phap");
-		return  0;
+int  isEmpty(Stack& st) {
+	if (!st.top) {
+		return 1;
 	}
-	p->coeff = coeff;
-	p->degree = degree;
+	return 0;
+}
+void show_Menu() {
+	cout << "\n-----------------------------\n";
+	cout << "\n 0: to end program ";
+	cout << "\n 1: created stack by hand ";
+	cout << "\n 2: created  a random stack  ";
+	cout << "\n 3: find 1 Stack Node in stack's list  ";
+	cout << "\n 4:convert interget to binary number";
+	cout << "\n 5:reverse string ";
+	cout << "\n-----------------------------\n";
+}
+StackNode* create_Stack(ItemType& x) {
+	StackNode* p = new StackNode;
+	if (!p) {
+		cout << "\n khong du bo nho de cap phap";
+		return 0;
+	}
+	p->info = x;
 	p->next = nullptr;
-	return p;
 }
-void show_SList(SList& sl) {
-	SNode* p = new SNode;
-	p = sl.Head;
-	while (p) {
-		printf("  % dx^%d", p->coeff, p->degree);
-		if (p->next) {
-			printf(" +");
-		}
-		p = p->next;
-		
-	}
-}
-void swap_SList(SNode* p, SNode* q) {
-	int temp_coeff = p->coeff;
-	int temp_degree = p->degree;
-	p->coeff = q->coeff;
-	p->degree = q->degree;
-	q->coeff = temp_coeff;
-	q->degree = temp_degree;
-}
-
-void sort_SList(SList& sl) {
-	for (SNode* p = sl.Head; p->next != nullptr; p = p->next) {
-		for (SNode* q = p->next; q != nullptr; q = q->next) {
-			if (p->degree < q->degree) {
-				swap_SList(p, q);
-			}
-		}
-	}
-}
-
-
-
-void insert_Head(SList& sl, SNode* p) {
-	if (p == NULL) {
-		printf( "\n khong du bo nho de cap phap");
-		return;
-	}
-	if (isEmpty(sl)) {
-		sl.Head = sl.Tail = p;
+void push(Stack& st, StackNode* p) {
+	if (isEmpty(st)) {
+		st.top = p;
 	}
 	else {
-		p->next = sl.Head;
-		sl.Head = p;
-
+		p->next = st.top;
+		st.top = p;
 	}
 }
-void Simlify(SList& sl) {
-	if (isEmpty(sl)) {
-		return;
-	}
-	SNode* q = sl.Head;
-	SNode* p = q->next;
-	while (q->next && p->next) {
-		if (q->degree == p->degree) {
-			q->coeff += p->coeff;
-			SNode* temp = p;
-			p = p->next;
-			q->next = p;
-			delete temp;
-		}
-		else {
-			q = q->next;
-			p = p->next;
-		}
-	}
-}
-void insertInOrder(SList &sl, ItemType coeff, int degree) {
-	SNode* p = create_SNode(coeff, degree);
-	if (isEmpty(sl)) {
-		sl.Head = sl.Tail = p;
-	}
-	else {
-		SNode* curr = sl.Head;
-		while (curr->next && curr->next->degree >= degree) {
-			curr = curr->next;
-		}
-		p->next = curr->next;
-		curr->next = p;
-	}
-}
-void deleteMonomail(SList& sl, ItemType x) {
-	if (isEmpty(sl)) {
-		return;
-	}
-	if (sl.Head == sl.Tail && sl.Head->degree == x) {
-		sl.Head = sl.Tail = nullptr;
-		return;
-	}
-	SNode* q = sl.Head;
-	SNode* p =q->next;
-	SNode* current = sl.Head;
-	while (current != NULL && current->next != NULL) {
-		if (current->next->degree == x) {
-			SNode* temp = current->next;
-			current->next = current->next->next;
-			delete(temp);
-		}
-		else {
-			current = current->next;
-		}
-	}
-}
-void evaluatePolynomial(SList& sl, ItemType x) {
-	double result = 0.0;
-
-	SNode* p = sl.Head;
-	while (p ) {
-		result += p->coeff * pow(x, p->degree);
-		p = p->next;
-	}
-	printf("\n gia tri cua bieu thuc khi x = %d  la %.3f ", x, result);
-}
-
-void addPolynomials(SList& slp, SList& slq) {
-	SList slr;
-	initSList(slr); 
-
-	SNode* p = slp.Head;
-	SNode* q = slq.Head;
-
-	while (p && q) {
-		if (p->degree == q->degree) {
-			ItemType x = p->coeff + q->coeff;
-			SNode* newNode = create_SNode(x, p->degree);
-			insert_Head(slr, newNode);
-			p = p->next;
-			q = q->next;
-		}
-		else if (p->degree > q->degree) {
-			
-			insert_Head(slr, create_SNode(p->coeff, p->degree));
-			p = p->next;
-		}
-		else {
-			
-			insert_Head(slr, create_SNode(q->coeff, q->degree));
-			q = q->next;
-		}
-	}
-
-	
-	while (p) {
-		insert_Head(slr, create_SNode(p->coeff, p->degree));
-		p = p->next;
-	}
-	while (q) {
-		insert_Head(slr, create_SNode(q->coeff, q->degree));
-		q = q->next;
-	}
-
-	Simlify(slr); 
-	printf("\n sau khi Q+P = \n");
-	show_SList(slr);
-}
-
-
-void createSList_ByRandom(SList& sl) {
+void createStack_ByHand(Stack& st) {
 	int n;
 	do {
-		printf("\n nhap do dai cua linked list: ");
-		scanf_s("%d", &n);
+		cout << "\n nhap do dai cua stack: ";
+		cin >> n;
+	} while (n <= 0);
+	for (int i = 0; i < n; i++) {
+		ItemType x;
+		cout << "\n nhap gia tri cua phan tu cua Stack ";
+		cin >> x;
+		StackNode* p = create_Stack(x);
+		push(st, p);
+	}
+}
+void createStack_Random(Stack& st) {
+	int n;
+	do {
+		cout << "\n nhap do dai cua stack: ";
+		cin >> n;
 	} while (n <= 0);
 	srand((unsigned)time(NULL));
 	for (int i = 0; i < n; i++) {
-		ItemType coeff = rand() % 199;
-		ItemType  degree = rand() % 10;
-		SNode* p = create_SNode(coeff,degree);
-		insert_Head(sl, p);
+		ItemType x = rand() % 199 - 99;
+		StackNode* p = create_Stack(x);
+		push(st, p);
 	}
 }
-void subtractPolynomials(SList& slp, SList& slq) {
-	SList slr;
-	initSList(slr);
-
-	SNode* p = slp.Head;
-	SNode* q = slq.Head;
-
-	while (p && q) {
-		if (p->degree == q->degree) {
-			ItemType x = p->coeff - q->coeff; 
-			SNode* newNode = create_SNode(x, p->degree);
-			insert_Head(slr, newNode);
-			p = p->next;
-			q = q->next;
-		}
-		else if (p->degree > q->degree) {
-			insert_Head(slr, create_SNode(p->coeff, p->degree));
-			p = p->next;
-		}
-		else {
-			insert_Head(slr, create_SNode(q->coeff, q->degree));
-			q = q->next;
-		}
+int pop(Stack& st, ItemType& x) {
+	StackNode* p = new StackNode;
+	if (isEmpty(st) == 1) {
+		return 0;
 	}
-
+	p = st.top;
+	st.top = st.top->next;
+	x = p->info;
+	delete(p);
+	return 1;
+}
+void show_Stack(Stack st) {
+	if (isEmpty(st)) {
+		cout << "\n Stack is empty ";
+		return;
+	}
+	cout << "\n Stack : ";
+	StackNode* temp = st.top;
+	while (temp) {
+		cout << temp->info << "\t ";
+		temp = temp->next;
+	}
+}
+static StackNode* FindStack(Stack& st, ItemType x) {
+	if (isEmpty(st)) {
+		cout << "\n can't find the stack node";
+		return NULL;
+	}
+	StackNode* p = st.top;
 	while (p) {
-		insert_Head(slr, create_SNode(p->coeff, p->degree));
+		if (p->info == x) {
+			return p;
+		}
 		p = p->next;
 	}
-
-	while (q) {
-		
-		insert_Head(slr, create_SNode(q->coeff, q->degree));
-		q = q->next;
+	return NULL;
+}
+void reverse_Stack(char* str) {
+	ItemType ch;
+	StackNode* p;
+	Stack st;
+	initStack(st);
+	for (int i = 0; i < strlen(str); i++) {
+		p = create_Stack(str[i]);
+		push(st, p);
 	}
+	cout << "\n after reversing: ";
+	while (true) {
+		if (pop(st, ch) == 0) {
+			break;
+		}
+		cout << ch; // Move cout outside the loop
+	}
+}
+void showStackNode(StackNode* p) {
+	cout << "\n Stack just created is : " << p->info;
+}
+bool check_Bracket(char* bracket) {
+	char check;
+	Stack st;
+	initStack(st);
+	for (int i = 0; i < strlen(bracket); i++) {
+		char x = bracket[i];
+		if (x == '(' || x == '{' || x == '[') {
+			StackNode* p = create_Stack(bracket[i]);
+			push(st, p);
+		}
+		else if (!isEmpty(st) && pop(st, check)) {
+			if ((x == ')' && check != '(') ||
+				(x == '}' && check != '{') ||
+				(x == ']' && check != '[')) {
+				return false;
+			}
+		}
+		else {
+			return false;
+		}
+	}
+	return isEmpty(st);
+}
 
-	printf("\n P - Q = \n");
-	show_SList(slr);
+//void BinaryNumber(ItemType x, int a) {
+//	Stack st;
+//	initStack(st);
+//	int temp = x;
+//	while (temp > 0) {
+//		int x = temp % a;
+//		StackNode* p = create_Stack(x);
+//		push(st, p);
+//		temp /= a;
+//	}
+//	while (true) {
+//		if (pop(st, x) == 0) {
+//			break;
+//		}
+//		if (x < 10) {
+//			cout << x;
+//		}
+//		else {
+//			switch (x) {
+//
+//			case 10:
+//				cout << "A";
+//				break;
+//			case 11:
+//				cout << "B";
+//				break;
+//			case 12:
+//				cout << "C";
+//				break;
+//			case 13:
+//				cout << "D";
+//				break;
+//			case 14:
+//				cout << "E";
+//				break;
+//			case 15:
+//				cout << "F";
+//				break;
+//			}
+//		}
+//	}
+//}
+void bai1() {
+	int choice;
+	Stack st;
+	initStack(st);
+	do {
+		show_Menu();
+		cout << "\n Enter your choice: ";
+		cin >> choice;
+		switch (choice) {
+		case 1:
+			createStack_ByHand(st);
+			show_Stack(st);
+			break;
+		case 2:
+			createStack_Random(st);
+			show_Stack(st);
+			break;
+			/*case 3: {
+				ItemType x;
+				cout << "\n Enter the value to search for: ";
+				cin >> x;
+				StackNode* p = FindStack(st, x);
+				if (p) {
+					cout << "\n" << p->info << " has inside  Stack Node";
+				}
+				else {
+					cout << "\n" << x << " doesn't exist inside Stack Node";
+				}
+				break;
+			}*/
+			/*case 4:
+				int a;
+				ItemType X;
+				do {
+					cout << "\n enter integer: ";
+					cin >> X;
+				} while (X <= 0);
+				do {
+					cout << "\n enter base: ";
+					cin >> a;
+				} while (a <= 1);
+				BinaryNumber(X, a);
+				break;*/
+		case 5:
+			cin.ignore();
+			char str[50];
+			cout << "\ enter the string you want reverse:  \n";
+			cin.getline(str, 50);
+			reverse_Stack(str);
+			break;
+		case 6:
+			cin.ignore();
+			char bracket[100];
+			cout << "\n enter the bracket to check! ";
+			cin.getline(bracket, 100);
+			if (check_Bracket(bracket)) {
+				cout << "\n the expression is balanced ";
+			}
+			else {
+				cout << "\n the expression isn't balanced ";
+			}
+			break;
+		}
+
+	} while (choice != 0);
 }
 
 int main() {
-	SList sl;
-	initSList(sl);
-	createSList_ByRandom(sl);
-	show_SList(sl);
-	sort_SList(sl);
-	show_SList(sl);
-	printf("\n giam dan bac mu ");
-	show_SList(sl);
-	Simlify(sl);
-	printf("\n toi gian ");
-	show_SList(sl);
-	insertInOrder(sl, 5, 3);
-	printf("\n sau khi them %dx^%d \n", 5, 3);
-	show_SList(sl);
-	deleteMonomail(sl, 2);
-	printf("\n sau khi xoa  mu %d \n",2);
-	show_SList(sl);
-	evaluatePolynomial(sl,8);
-	SList slp;
-	initSList(slp);
-	printf("\nda thuc ban dau la P:\n");
-	createSList_ByRandom(slp);
-	show_SList(slp);
-	SList slq;
-	initSList(slq);
-	printf("\nda thuc ban dau la Q:\n");
-	createSList_ByRandom(slq);
-	show_SList(slq);
-	//addPolynomials(slp, slq);
-	subtractPolynomials(slp, slq);
+	bai1();
 	return 0;
 }
